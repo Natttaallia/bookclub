a221f45e1a205a373fe75bccc3850b9eafdaa33c
nat <k@gmail.com>
2018-12-13 20:25:48 +0200

    add personalcabinet

--- app/controllers/PersonalController.php
+++ app/controllers/PersonalController.php
@@ -5,13 +5,19 @@ namespace App\controllers;
 
 	use Zend\Diactoros\Response\HtmlResponse;
 	use duncan3dc\Laravel\BladeInstance;
+	use Illuminate\Database\Capsule\Manager as DB;
+
 
 
 
 class PersonalController{
 
 
-
+private $count;
+	private $page;
+	private $start;
+	private $allbooks;
+	private $page_count;
 
 
 public function __invoke($request)
@@ -20,11 +26,30 @@ public function __invoke($request)
 				__DIR__ . "/../../pages", 
 				__DIR__ . "/../../cache/pages"
 			);
-
 	session_start();
+
+$id=DB::table('users')
+					->where('login', '=', $_SESSION['username'])
+					->value('id');
+					// ->get();
+				// var_dump($id);
+$data=DB::table('books')
+					->where('user_id', '=', $id)
+					->get()
+					->toArray();
+
+
+	foreach ($data as $key => $value) {
+		$data[$key]= (array) $value;
+		$author=$this->getStringById($data[$key]["author_id"],'name','authors');
+	$data[$key]+=["author_name"=>$author];
+	$category=$this->getStringById($data[$key]["category_id"],'title','categories');
+	$data[$key]+=["category_name"=>$category];
+	}
 	if(isset($_SESSION['username']))	
 		$content=$blade->render('personal', [
-					'name' => $_SESSION['username']
+					'name' => $_SESSION['username'],
+					'data'=>$data
 				]);
 	else
 		$content=$blade->render('error', [
@@ -37,7 +62,12 @@ public function __invoke($request)
 
 
 }
-
+private function getStringById($id,$column,$table){
+		$pas=DB::table($table)->where('id', '=', $id)->get()->toArray();
+		foreach ($pas as $key => $value) 
+			$pas[$key]= (array) $value;
+		return $pas[0][$column];
+	}
 
 }
 
--- pages/home.blade.php
+++ pages/home.blade.php
@@ -35,57 +35,7 @@
 
 </div>
 
-<div style='display:flex;justify-content: center;' >	
- <ul class='pagination'>
-    
-
-    @if($page==0) <li class='disabled'>
-    @else <li class='waves-effect'>
-    @endif
-
-    <a 
-    @if($page!=0) href='/$page'
-    @endif 
-    ><i class='material-icons'>chevron_left</i></a></li>
- 	
-
-
- 	 @for ($i=1; $i <= $page_count; $i++)
-
-
- 	 @if ($i==1 || $i==$page_count || (($i<=$page+1) && ($i>=$page-5)) || (($i>=$page+1) && ($i<=$page+6)))	
-   <li 
-      @if ($page+1==$i) class='active'
-      @else class='waves-effect'
-      @endif
-   ><a href='/{{ $i }}'>{{ $i }}</a></li>
- 
-		
-		@elseif($i<$page && $flag_left)
-		
-			<li class='waves-effect'><a>...</a></li>
-		{{ $flag_left=!$flag_left }}
-		@elseif($i>$page && $flag_right)
-		
-			<li class='waves-effect'><a>...</a></li>
-    {{ $flag_right=!$flag_right }}
-		
-    @endif
-
-
-	 @endfor
-    
-    <li 
-    @if($page+1==$page_count) class='disabled'
-    @else class='waves-effect'
-    @endif
-    ><a 
-    @if($page+1!=$page_count) 
-     href='/{{ $page+2}}'
-     @endif
-     ><i class='material-icons'>chevron_right</i></a></li>
-  </ul>
-</div>
+@include('pagination');
 
 
 
